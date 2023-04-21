596b69be5f514ed3bdb735158c65543dc6af21d9
Tamim <tamimyasuo@gmail.com>
2023-04-21 01:16:04 -0700

    Created a new Stats component that generates each event Stat individually. Updated the previous Stat component to use props set from the Stats component.

--- dashboard/src/components/Stats.jsx
+++ dashboard/src/components/Stats.jsx
@@ -1,39 +1,28 @@
-import { useEffect, useState } from 'react'
+import Stat from './Stat';
 
 const Stats = () => {
-    const [event, setEvent] = useState([])
-
-    useEffect(() => {
-        fetch('http://localhost/processing/stats')
-        .then(res => res.json())
-        .then(res => { 
-            setEvent(res) 
-        })
-    }, [])
+    const state = {
+        stats : [
+            {stat_name: 'Number of Buys', stat_var: "num_buys", hasDollar: false},
+            {stat_name: 'Number of Sells', stat_var: "num_sells", hasdollar: false},
+            {stat_name: 'Max Buy Price', stat_var: "max_buy_price", hasDollar: true},
+            {stat_name: 'Max Sell Price', stat_var: "max_sell_price", hasDollar: true},
+            {stat_name: 'Last Updated', stat_var: "last_updated", hasDollar: false}
+        ]
+    }
 
     return (
-        <div className="stats">
+        <div className='all-stats'>
             <h2>Latest Statistics</h2>
-            <div className='events-stats-wrapper'>
-                <div className='events-stats'>
-                    <div className='stats-params'>
-                        <p className='stats-param'>Number of Buys:</p>
-                        <p className='stats-param'>Number of Sells:</p>
-                        <p className='stats-param'>Max Buy Price:</p>
-                        <p className='stats-param'>Max Sell Price:</p>
-                        <p className='stats-param'>Last Updated:</p>
-                    </div>
-                    <div className='stats-values'>
-                        <p className='stats-value'>{event.num_buys}</p>
-                        <p className='stats-value'>{event.num_sells}</p>
-                        <p className='stats-value'>{event.max_buy_price}$</p>
-                        <p className='stats-value'>{event.max_sell_price}$</p>
-                        <p className='stats-value'>{event.last_updated}</p>
-                    </div>
-                </div>
-            </div>    
+            <div className='all-stats-container'>
+                {state.stats.map((stat) => (
+                    <Stat stat_name={stat.stat_name} 
+                    stat_var={stat.stat_var} 
+                    dollar={stat.hasDollar ? "$" : ""} />
+                ))}
+            </div>
         </div>
-    )   
-}
+    );
+};
 
-export default Stats
+export default Stats;
